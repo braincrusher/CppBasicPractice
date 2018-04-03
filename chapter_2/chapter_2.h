368a4e78c01c0f15d4739c2653370ae584e96fd5
Joe <247946975@qq.com>
2018-04-03 14:37:15 +0800

    add chapter_2 file

---
+++ chapter_2/chapter_2.h
@@ -0,0 +1,41 @@
+#ifndef _CHAPTER_2_H_
+#define _CHAPTER_2_H_
+#include <cmath>
+#include <iostream>
+//Program list
+//2.1
+void myfirst(void);
+//2.2
+void carrots(void);
+//2.3
+void getinfo(void);
+//2.4
+void sqrt(void);
+//2.5
+void simon(int);
+void ourfunc(void);
+//2.6
+void convert(void);
+int stonetolb(int);
+//Homework
+//2.7.1
+void name_addr(void);
+//2.7.2
+void trans(void);
+//2.7.3
+void imp(void);
+void child1(void);
+void child2(void);
+//2.7.4
+void ageTrans(void);
+//2.7.5
+void batman(void);
+float superman(float);
+//2.7.6
+void ironman(void);
+double thor(double);
+//2.7.7
+void zergling(void);
+void hydralisk(int , int);
+//These function should add something.
+#endif
---
+++ chapter_2/main.cpp
@@ -0,0 +1,31 @@
+#include "chapter_2.h"
+int main(void)
+{
+    //myfirst();
+
+    //carrots();
+
+    //getinfo();
+
+    //sqrt();
+
+    //ourfunc();
+
+    //convert();
+    //
+    name_addr();
+
+    trans();
+
+    imp();
+
+    ageTrans();
+
+    batman();
+
+    ironman();
+
+    zergling();
+
+    return 0;
+}
